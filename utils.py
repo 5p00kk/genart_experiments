d6ed332747d25c0f2030abe120ba3839a611185a
5p00kk <szymon.kowalewski@gmail.com>
2022-05-09 22:42:28 +0200

    add get_hsv

--- utils.py
+++ utils.py
@@ -1,4 +1,11 @@
 import math
+import numpy as np
+import cv2
 
 def sigmoid(x):
   return 1 / (1 + math.exp(-x))
+
+def get_hsv(r, g, b):
+  rgb = np.uint8([[[b, g, r]]])
+  hsv = cv2.cvtColor(rgb, cv2.COLOR_BGR2HSV)
+  return hsv
